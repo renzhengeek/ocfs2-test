renzhengeek/ocfs2-test

commit cf64e6eae02eee34bffa7fd634891bbb60c6a72d

Handle the case when a symbolic link device is given
It's a usual case that the shared disk is given in symbolic
name, like iscsi disk "/dev/disk/by-path/disk". So work out
the typical device name in such case.

Signed-off-by: Eric Ren <zren@suse.com>

programs/discontig_bg_test/discontig_runner.sh
@@ -126,6 +126,11 @@ function f_setup()
 	if [ -z "${DEVICE}" ];then
 		f_usage
 	fi	
+
+	# if a symbollink is given, work out the typical device name, like /dev/sda
+	if [ -L ${DEVICE} ];then
+		DEVICE=`readlink -f ${DEVICE}`
+	fi
 	
 	if [ -z "${MOUNT_POINT}" ];then
 		f_usage

programs/python_common/multiple_run.sh
@@ -135,6 +135,19 @@ f_setup()
 
 	f_getoptions $*
 
+	if [ -z ${DEVICE} ] ; then
+		${ECHO} "ERROR: No device"
+		f_usage
+	elif [ ! -b ${DEVICE} ] ; then
+		${ECHO} "ERROR: Invalid device ${DEVICE}"
+		exit 1
+	fi
+
+	# if a symbollink is given, work out the typical device name, like /dev/sda
+	if [ -L ${DEVICE} ];then
+		 DEVICE=`readlink -f ${DEVICE}`
+	fi
+
 	if [ -z "${MOUNT_POINT}" ];then
                 f_usage
         fi

programs/python_common/single_run-WIP.sh
@@ -1095,12 +1095,18 @@ do
     		?) usage;;
   	esac
 done
+
 if [ -z ${DEVICE} ] ; then
 	${ECHO} "ERROR: No device"
 	usage
 elif [ ! -b ${DEVICE} ] ; then
 	${ECHO} "ERROR: Invalid device ${DEVICE}"
 	exit 1
+fi
+
+# if a symbollink is given, work out the typical device name, like /dev/sda
+if [ -L ${DEVICE} ];then
+	DEVICE=`readlink -f ${DEVICE}`
 fi
 
 if [ -z ${MOUNTPOINT} ] ; then

commit 63dba33589199effc183f8204af40ec57b4739ec

discontig bg: handle symbol link device
The name of iscsi shared disk can be different among nodes.

Fixes: cf64e6eae02e (Handle the case when a symbolic link device is
given)
Signed-off-by: Eric Ren <zren@suse.com>

programs/discontig_bg_test/discontig_runner.sh
@@ -126,11 +126,6 @@ function f_setup()
 	if [ -z "${DEVICE}" ];then
 		f_usage
 	fi	
-
-	# if a symbollink is given, work out the typical device name, like /dev/sda
-	if [ -L ${DEVICE} ];then
-		DEVICE=`readlink -f ${DEVICE}`
-	fi
 	
 	if [ -z "${MOUNT_POINT}" ];then
 		f_usage
@@ -182,13 +177,19 @@ function f_get_disk_usage()
 {
 	local DISK_FREE=
 	local DISK_FREE_M=
+	local DISK_NAME=
+
+	# if a symbollink is given, work out the typical device name, like /dev/sda
+	if [ -L ${DEVICE} ];then
+		DISK_NAME=`readlink -f ${DEVICE}`
+	fi
 
         f_LogMsg ${LOG_FILE} "Calculate the disk total and free size"
 
         DISK_FREE=`df |grep ${MOUNT_POINT}|awk '{print $4}'`
 
         if [ -z "${DISK_FREE}" ]; then
-                DISK_FREE=`df |grep ${DEVICE}|awk '{print $4}'`
+                DISK_FREE=`df |grep ${DISK_NAME}|awk '{print $4}'`
         fi
 
         DISK_FREE_M=`echo ${DISK_FREE}/1024|bc`